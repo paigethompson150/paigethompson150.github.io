27d111347a833934a6a9410f749152a4644751b6
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2023-10-01 01:39:47 -0400

    Creating project and adding header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+export default function Header() {
+  return <header className="App-header">
+      <ul>
+        <li cla>Paige Thompson</li>
+        <li>About</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+        <li>|</li>
+        <li>Travels</li>
+      </ul>
+  </header>
+}
