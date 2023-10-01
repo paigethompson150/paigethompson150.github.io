690989532196e8c7505b4d7a8b8279adb235f8f9
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2023-10-01 02:54:33 -0400

    Adding home screen rough draft

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@
 export default function Header() {
   return <header className="App-header">
       <ul>
-        <li cla>Paige Thompson</li>
+        <li>Paige Thompson</li>
         <li>About</li>
         <li>Portfolio</li>
         <li>Contact</li>
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return <div className="Home">
+      <span className="dot"></span>
+      <span className="dot"></span>
+      <span className="dot"></span>
+
+      <div className="home-intro">
+        <h4>Hey, my name is</h4>
+        <h1>Paige Thompson</h1>
+        <h4>Learn a little bit more about me</h4>
+      </div>
+  </div>
+}
