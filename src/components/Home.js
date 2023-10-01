f82be2e53beacb4c625b6d3522f59b4698a93b64
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2023-10-01 03:23:44 -0400

    Changing width of page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 
 export default function Home() {
   return <div className="Home">
+    <div className="dots">
       <span className="dot"></span>
       <span className="dot"></span>
       <span className="dot"></span>
+    </div>
 
       <div className="home-intro">
         <h4>Hey, my name is</h4>
