0f30f1da54a6ec687ec2bcc94c8e8c72b90c8e82
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2023-10-01 21:28:54 -0400

    adding highlight to header selected items

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header setPage={setPage}/>
+      <Header setPage={setPage} page={page}/>
       <div>
         {pages[page]}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,16 @@ import { useState } from "react"
 import Home from "./Home"
 
 export default function Header(props) {
-  const { setPage } = props
-
-  const [clicked, setClicked] = useState(false)
-  const handleClicked = () => {
-    setClicked(!clicked)
-  }
+  const { setPage, page } = props
 
   return <header className="App-header">
       <ul>
-        <li id="title" onClick={() => {setPage(0)}}>Paige Thompson</li>
-        <li onClick={() => {setPage(1)}}>About</li>
-        <li onClick={() => {setPage(2)}}>Portfolio</li>
-        <li onClick={() => {setPage(3)}}>Contact</li>
+        <li className={page === 0 ? "heading" : ""} id="title" onClick={() => {setPage(0)}}>Paige Thompson</li>
+        <li className={page === 1 ? "heading" : ""} onClick={() => {setPage(1)}}>About</li>
+        <li className={page === 2 ? "heading" : ""} onClick={() => {setPage(2)}}>Portfolio</li>
+        <li className={page === 3 ? "heading" : ""} onClick={() => {setPage(3)}}>Contact</li>
         <li>|</li>
-        <li onClick={() => {setPage(4)}}>Travels</li>
+        <li className={page === 4 ? "heading" : ""} onClick={() => {setPage(4)}}>Travels</li>
       </ul>
   </header>
 }
