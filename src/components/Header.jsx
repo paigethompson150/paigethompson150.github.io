5dccc54294d8e5d2448abb520eed0a06cc661ce8
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2023-10-03 04:11:00 -0400

    changing up the design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,11 @@ export default function Header(props) {
   return <header className="App-header">
       <ul>
         <li className={page === 0 ? "heading" : ""} id="title" onClick={() => {setPage(0)}}><img src={pageEmoji} alt="page emoji"/></li>
-        <li className={page === 1 ? "heading" : ""} onClick={() => {setPage(1)}}>About</li>
-        <li className={page === 2 ? "heading" : ""} onClick={() => {setPage(2)}}>Portfolio</li>
-        <li className={page === 3 ? "heading" : ""} onClick={() => {setPage(3)}}>Contact</li>
+        <li className={[page === 1 ? "heading" : "","underlinedHeader"].join(' ')} onClick={() => {setPage(1)}}>About</li>
+        <li className={[page === 2 ? "heading" : "","underlinedHeader"].join(' ')} onClick={() => {setPage(2)}}>Portfolio</li>
+        <li className={[page === 3 ? "heading" : "","underlinedHeader"].join(' ')} onClick={() => {setPage(3)}}>Contact</li>
         <li>|</li>
-        <li className={page === 4 ? "heading" : ""} onClick={() => {setPage(4)}}>Travels</li>
+        <li className={[page === 4 ? "heading" : "","underlinedHeader"].join(' ')} onClick={() => {setPage(4)}}>Travels</li>
       </ul>
   </header>
 }
