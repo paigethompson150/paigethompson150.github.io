27eb96d2fdca6db414e1394fa57f47e1c83325b2
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2023-10-03 18:01:00 -0400

    adding overlay

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,7 +5,7 @@ import gamenight from '../images/gamenight.png';
 import cos from '../images/cos.png';
 import ramen from '../images/ramen.png';
 import portfolio from '../images/portfolio.png';
-import tweet from '../images/tweet.png';
+import tweet from '../images/tweet.jpeg';
 import ticTacToe from '../images/tictactoe.gif';
 import dashboard from '../images/dashboard.png';
 import signUp from '../images/SignUp.png'
@@ -13,20 +13,20 @@ import signUp from '../images/SignUp.png'
 export default function Portfolio() {
   let leftProjects = [
     {key: 0, type: "Web", name:"Pagu Ramen", image:ramen, desc: "Restaurant landing page"},
-    {key: 1, type: "Web", name:"Curse of Strahd", image:cos, desc: "Book Tracking Site"},
     {key: 2, type: "iOS", name:"GameNight", image:gamenight, desc: "Created an iOS app"},
-    {key: 3, type: "Web", name:"Portfolio", image:portfolio, desc: "Designed a reactive native app"},
-    {key: 7, type: "Web", name:"Tic-Tac-Toe", image:ticTacToe, desc: "Designed a reactive native app"}
+    {key: 7, type: "Web", name:"Tic-Tac-Toe", image:ticTacToe, desc: "Designed a reactive native app"},
+    {key: 5, type: "Design", name:"Admin Dashboard", image:dashboard, desc: "Designed a reactive native app"},
   ]
   let rightProjects = [
+    {key: 1, type: "Web", name:"Curse of Strahd", image:cos, desc: "Book Tracking Site"},
+    {key: 3, type: "Web", name:"Portfolio", image:portfolio, desc: "Designed a reactive native app"},
     {key: 4, type: "iOS", name:"Tweeter", image:tweet, desc: "Designed a reactive native app"},
-    {key: 5, type: "Design", name:"Admin Dashboard", image:dashboard, desc: "Designed a reactive native app"},
     {key: 6, type: "Design", name:"Sign Up Form", image:signUp, desc: "Designed a reactive native app"}
   ]
   const [selectedProjectType, setSelectedProjectType] = useState("All");
   const [selectedProject, setSelectedProject] = useState(-1);
-  const leftProjectsList = leftProjects.map((project, index) => <Project className={selectedProjectType === project.type || selectedProjectType === "All" ? "shown" : "hkeyden"} obj={project} onClick={() => setSelectedProject(project)}/>);
-  const rightProjectsList = rightProjects.map((project, index) => <Project className={selectedProjectType === project.type || selectedProjectType === "All" ? "shown" : "hkeyden"} obj={project} onClick={() => setSelectedProject(project)}/>);
+  const leftProjectsList = leftProjects.map((project, index) => <Project className={selectedProjectType === project.type || selectedProjectType === "All" ? "shown" : "hidden"} obj={project} onClick={() => setSelectedProject(project)}/>);
+  const rightProjectsList = rightProjects.map((project, index) => <Project className={selectedProjectType === project.type || selectedProjectType === "All" ? "shown" : "hidden"} obj={project} onClick={() => setSelectedProject(project)}/>);
 
   const newRef = useRef(null);
   useEffect(() => {
@@ -70,5 +70,6 @@ export default function Portfolio() {
           <h2>{selectedProject.desc}</h2>
         </div>
       </div>
+      <div className={selectedProject !== -1 ? "overlay" : ""}></div>
   </div>
 }
