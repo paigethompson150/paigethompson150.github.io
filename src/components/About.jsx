8ccb99a5d3dd67ad989532807657d376bcb95e68
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2023-10-05 00:42:49 -0400

    Redesigning experience descriptions

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,25 +8,26 @@ export default function About() {
     {
       key:0, company: "Guardian Life", date: "Jun. 2023 - Aug. 2023", 
       title: "Mobile/iOS Developer Intern",
-      role1: "· Collaborated with lead engineers and UX team to create a Proof of Concept iOS Application to be used internally by Guardian Life employees.", 
-      role2: "· Rewrote UIKit components into SwiftUI and added notification badging to the tab bar of Guardian Life’s Accounts & Policies application. Participated in daily stand-ups, PI planning, and refinements."
+      role1: "Collaborated with lead engineers and UX team to create a Proof of Concept iOS Application to be used internally by Guardian Life employees.", 
+      role2: "Rewrote UIKit components into SwiftUI and added notification badging to the tab bar of Guardian Life’s Accounts & Policies application. Participated in daily stand-ups, PI planning, and refinements."
     },
     {
       key:1,company:"Bose", date:"Jan. 2023 - Jun. 2023", 
       title: "Mobile Application Engineer Co-op",
-      role1: "· Created a Proof of Concept screen for the Bose Music application to initiate research for the new Spatial Audio demo feature, and demonstrated findings to the Mobile application team.",
-      role2: "· Enhanced accessibility, debugged bluetooth connection issues and integrated upcoming products into the Bose Music app."
+      role1: "Created a Proof of Concept screen for the Bose Music application to initiate research for the new Spatial Audio demo feature, and demonstrated findings to the Mobile application team.",
+      role2: "Enhanced accessibility, debugged bluetooth connection issues and integrated upcoming products into the Bose Music app."
     },
     {
       key:2, company:"Boynton Insurance", date:"Jul. 2022 - Dec. 2022",
       title: "SEO/Digital Marketing Associate",
-      role1: "· Updated SEO strategy and restructured website to increase page visits by 60%. Designed new web pages and blog posts.",
+      role1: "Updated SEO strategy and restructured website to increase page visits by 60%.",
+      role2: "Designed and deployed clients intake forms, web pages, and email campaigns."
     },
     {
       key:3, company:"Girls Who Code", date:"Jun. 2022 - Aug. 2022", 
       title: "Computer Science TA",
-      role1: "· Maintained an exciting and accessible virtual classroom environment for a highly diverse group of 50-60 high school girls and non-binary students.",
-      role2: "· Led classroom lectures on HTML, CSS, JavaScript, and OOP concepts. Individually mentored 10-15 of my own students and created engaging sisterhood activities for us to play."
+      role1: "Maintained an exciting and accessible virtual classroom environment for a highly diverse group of 50-60 high school girls and non-binary students.",
+      role2: "Led classroom lectures on HTML, CSS, JavaScript, and OOP concepts. Individually mentored 10-15 of my own students and created engaging sisterhood activities for us to play."
     }
   ]
   const experienceList = experiences.map((e) => <Experience exp={e} />)
