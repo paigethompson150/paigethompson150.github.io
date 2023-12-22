9c190e4d6ed1d91caac6807466999c6301020ee7
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2023-12-22 01:22:17 -0500

    removing travels section for now

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import Home from './components/Home';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
-import Travels from './components/Travels';
 import React, { useState, useEffect } from 'react';
 
 function App() {
   const [page, setPage] = useState(0);
-  const pages = [<Home onClick={() => setPage(1)}/>, <About/>, <Portfolio/>, <Contact/>, <Travels/>]
+  const pages = [<Home onClick={() => setPage(1)}/>, <About/>, <Portfolio/>, <Contact/>]
 
   return (
     <div className="App">
